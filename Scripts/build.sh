00fb9c02d507190de58478b4d7f1aec6b1309633
Roshan Nindrai <roshannindrai@Roshans-MacBook-Pro.local>
2017-05-28 17:09:21 -0400

    Added build script and linker flags to use MobileDevice private Framework

---
+++ Scripts/build.sh
@@ -0,0 +1,1 @@
+swift build -Xlinker -F/System/Library/PrivateFrameworks/ -Xlinker -v -Xlinker -framework -Xlinker MobileDevice -Xswiftc -import-objc-header -Xswiftc Sources/MayonFramework/Platform/Apple/mobiledevice/MobileDevice-Bridging-Header.h
