e576fceec66a0944ad185e0c53dca9d8c44a48c9
Roshan Nindrai <roshannindrai@Roshans-MacBook-Pro.local>
2017-05-28 17:23:27 -0400

    updated build script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,1 +1,34 @@
-swift build -Xlinker -F/System/Library/PrivateFrameworks/ -Xlinker -v -Xlinker -framework -Xlinker MobileDevice -Xswiftc -import-objc-header -Xswiftc Sources/MayonFramework/Platform/Apple/mobiledevice/MobileDevice-Bridging-Header.h
+#!/bin/bash
+
+export SCRIPT_DIR=$(dirname "$0")
+
+##
+## Starts here
+##
+
+main ()
+{
+    build
+}
+
+##
+## Build command with custom options for linker flags and swift compiler flags ex bridging headers
+##
+
+build ()
+{
+
+    # Adding linker flag to private frameworks
+    # Specifying the required framework
+    # Adding Objc bridging header
+
+    swift build -Xlinker -v \
+    -Xlinker -F/System/Library/PrivateFrameworks \
+    -Xlinker -framework -Xlinker MobileDevice \
+    -Xswiftc -import-objc-header -Xswiftc Sources/MayonFramework/Platform/Apple/mobiledevice/MobileDevice-Bridging-Header.h
+
+}
+
+export -f build
+
+main
